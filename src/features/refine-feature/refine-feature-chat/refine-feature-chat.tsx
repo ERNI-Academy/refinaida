10e21658863de1521aa5a6ce1d730b6d7f5e7d37
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2024-09-24 16:45:33 +0200

    chore: update pages and features files

---
+++ src/features/refine-feature/refine-feature-chat/refine-feature-chat.tsx
@@ -0,0 +1,17 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const RefineFeatureChat = () => (
+  <Card className="w-3/5">
+    <CardHeader>
+      <CardTitle>Refine a feature</CardTitle>
+      <CardDescription>What feature does your solution need?</CardDescription>
+    </CardHeader>
+  </Card>
+);
+
+export default RefineFeatureChat;
--- src/pages/new-feature-page.tsx
+++ src/pages/new-feature-page.tsx
@@ -1,43 +1,5 @@
-import { Container } from "@/components/layout/container";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { useAppStore } from "@/hooks/use-app-store";
-import { useNavigate } from "react-router-dom";
+import NewFeature from "@/features/new-feature/new-feature";
 
-export const NewFeaturePage = () => {
-  const navigate = useNavigate();
-  const { setFeature } = useAppStore();
-  return (
-    <Container>
-      <Card>
-        <CardHeader className="mb-4">
-          <CardTitle className="text-5xl font-semibold text-center">
-            Let's refine a feature
-          </CardTitle>
-          <CardDescription className="text-lg text-gray-500 text-center">
-            What feature does your solution need?
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <div className="flex flex-col gap-4 items-center">
-            <Input
-              className="w-3/5"
-              placeholder="Adding user authentication"
-              onChange={(e) => setFeature(e.target.value)}
-            />
-            <Button className="w-2/6" onClick={() => navigate("/refine")}>
-              Start refining
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-    </Container>
-  );
-};
+const NewFeaturePage = () => <NewFeature />;
+
+export default NewFeaturePage;

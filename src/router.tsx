8de25a94bc5e6ef90d4079ee52a5c9a8b8fb2ecc
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2024-09-25 12:11:50 +0200

    feat(refineFeature): add new buttons and styles

--- src/router.tsx
+++ src/router.tsx
@@ -3,17 +3,22 @@ import { Root } from "./root";
 import NewFeaturePage from "@/pages/new-feature-page";
 import RefineFeaturePage from "@/pages/refine-feature-page";
 
+export const routes = {
+  default: "/",
+  refine: "/refine",
+};
+
 export const router = createMemoryRouter([
   {
     path: "/",
     element: <Root />,
     children: [
       {
-        path: "/",
+        path: routes.default,
         element: <NewFeaturePage />,
       },
       {
-        path: "/refine",
+        path: routes.refine,
         element: <RefineFeaturePage />,
       },
     ],

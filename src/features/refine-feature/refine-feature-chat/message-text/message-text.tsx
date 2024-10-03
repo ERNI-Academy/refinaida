6604e159c4e360199c0a7ddd5742d750c1a5fabe
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2024-10-03 17:36:46 +0200

    feat(refine-chat): code improvements

---
+++ src/features/refine-feature/refine-feature-chat/message-text/message-text.tsx
@@ -0,0 +1,69 @@
+import { Bot, User } from "lucide-react";
+import { memo, useEffect, useState } from "react";
+
+import { USER } from "@/features/refine-feature/refine-feature-chat/refine-feature-chat.const";
+import { Message } from "@/features/refine-feature/refine-feature-chat/refine-feature-chat.types";
+
+interface MessageTextProps {
+  message: Message;
+  index: any;
+  isLoading?: boolean;
+}
+
+const MESSAGE_THINKING = "Thinking";
+
+const MessageText = ({
+  message,
+  index,
+  isLoading = false,
+}: MessageTextProps) => {
+  const [loadingText, setLoadingText] = useState<string>(MESSAGE_THINKING);
+
+  console.log("renderMessage");
+
+  const isUser = message.sender === USER;
+
+  useEffect(() => {
+    let interval: any;
+
+    if (isLoading) {
+      interval = setInterval(() => {
+        setLoadingText((prev) => {
+          if (prev.includes("...")) return MESSAGE_THINKING;
+          return prev + ".";
+        });
+      }, 500);
+    } else {
+      setLoadingText(MESSAGE_THINKING);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isLoading]);
+
+  return (
+    // TODO: Remove index when the data will be properly
+    <div
+      key={index}
+      className={`flex mb-4  ${isUser ? "justify-end" : "justify-start"}`}
+    >
+      <div className="flex gap-2">
+        <div
+          className={`p-2 inline-block ${
+            isUser
+              ? "order-1 bg-blue-800 text-white rounded-except-br"
+              : "order-3 bg-gray-200 text-black rounded-except-bl"
+          }`}
+        >
+          {!isUser && isLoading ? loadingText : message.text}
+        </div>
+        <div className="flex items-end order-2">
+          {isUser ? <User className="h-6 w-6" /> : <Bot className="h-6 w-6" />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(MessageText);
---
+++ src/features/refine-feature/refine-feature-chat/refine-feature-chat.const.ts
@@ -0,0 +1,3 @@
+export const USER = "You";
+export const USER_AIDA = "AIDA";
+export const MESSAGE_THINKING = "Thinking";

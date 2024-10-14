d70b7c3c0f60d1f5abebff19f9aa8780610d1cea
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2024-10-14 14:39:58 +0200

    styles: update height and add more styles

--- src/features/backlog-feature/backlog-feature-description/backlog-feature-description.tsx
+++ src/features/backlog-feature/backlog-feature-description/backlog-feature-description.tsx
@@ -1,3 +1,5 @@
+import "./backlog-feature-description.scss";
+
 import MarkdownRenderer from "@/components/markdown/markdown";
 import { Card, CardHeader, CardTitle } from "@/components/ui/card/card";
 import { useAppStore } from "@/hooks/use-app-store";
@@ -6,7 +8,7 @@ const BacklogFeatureDescription = () => {
   const { currentRefineBacklog } = useAppStore();
 
   return (
-    <Card className="w-2/5">
+    <Card className="w-2/5 backlog-feature-description-wrapper overflow-y-auto">
       {currentRefineBacklog?.description && (
         <CardHeader className="w-full h-full p-10">
           <CardTitle className="mb-3">{currentRefineBacklog.title}</CardTitle>

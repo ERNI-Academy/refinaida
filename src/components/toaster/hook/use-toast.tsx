77290930229913354884c9036972892ed4cfd193
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2024-11-04 17:45:11 +0100

    feat(toast): improvement toast implementation

--- src/components/toaster/hook/use-toast.tsx
+++ src/components/toaster/hook/use-toast.tsx
@@ -90,8 +90,6 @@ export const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action;
 
-      // ! Side effects ! - This could be extracted into a dismissToast() action,
-      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId);
       } else {
@@ -137,9 +135,9 @@ function dispatch(action: Action) {
   });
 }
 
-type Toast = Omit<ToasterToast, "id">;
+type Toast = Omit<ToasterToast, "id"> & { duration?: number };
 
-function toast({ ...props }: Toast) {
+function toast({ duration = 4000, ...props }: Toast) {
   const id = genId();
 
   const update = (props: ToasterToast) =>
@@ -155,6 +153,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
+      duration: duration,
       onOpenChange: (open) => {
         if (!open) dismiss();
       },

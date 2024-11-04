ac188492f4ddf3923221f2e8d792ceecc68dc62f
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2024-11-04 13:54:30 +0100

    feat(toast): add new component toast and toaster hook

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import { Toaster } from "@/components/toaster/toaster.tsx";
+
 import { router } from "./router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    <Toaster />
   </StrictMode>
 );

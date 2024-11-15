40db9a9ae9d8819be760f44399e06a6c6b46c687
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2024-11-15 11:11:12 +0100

    feat: update dialog and added option to return to the header

--- src/components/shell.tsx
+++ src/components/shell.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from "react-i18next";
 
+import BackNewFeatureDialog from "@/features/new-feature/back-new-feature-dialog/back-new-feature-dialog";
+
 type ShellProps = {
   children: React.ReactNode;
 };
@@ -10,7 +12,13 @@ export const Shell = ({ children }: ShellProps) => {
   return (
     <div className="flex flex-col h-full w-full">
       <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4">
-        <h1 className="text-2xl uppercase font-mono">{t("header.title")}</h1>
+        <BackNewFeatureDialog
+          trigger={
+            <h1 className="text-2xl uppercase font-mono cursor-pointer">
+              {t("header.title")}
+            </h1>
+          }
+        />
       </header>
       <main className="h-full w-full flex-1 bg-muted/40">{children}</main>
     </div>

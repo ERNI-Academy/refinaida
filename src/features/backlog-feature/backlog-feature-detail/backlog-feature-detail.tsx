f68c630278ea54beaaea76d013ba047712da1cce
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2025-03-06 12:37:10 +0100

    feat(backlogFeature): improvement backlog detail list

--- src/features/backlog-feature/backlog-feature-detail/backlog-feature-detail.tsx
+++ src/features/backlog-feature/backlog-feature-detail/backlog-feature-detail.tsx
@@ -8,6 +8,7 @@ import { DetailType } from "@/features/backlog-feature/backlog-feature-detail-it
 import useGetDetailRefinedFeature from "@/features/backlog-feature/hooks/use-detail-refined-backlog";
 import { useAppStore } from "@/stores/use-app-store";
 import { useBacklogFeatureStore } from "@/stores/use-backlog-feature-store";
+import { RefinedBacklogDetailList } from "@/types/common";
 
 const BacklogFeatureDetail = () => {
   const { t } = useTranslation();
@@ -16,6 +17,25 @@ const BacklogFeatureDetail = () => {
   const { isLoadingDetail } = useBacklogFeatureStore();
 
   const { detailRefinedBacklog } = useGetDetailRefinedFeature();
+
+  const backlogFeatureDetailList: RefinedBacklogDetailList[] = [
+    {
+      type: DetailType.BUSINESS_VALUE,
+      value: detailRefinedBacklog?.details.businessValue || "",
+    },
+    {
+      type: DetailType.DESCRIPTION,
+      value: detailRefinedBacklog?.details.description || "",
+    },
+    {
+      type: DetailType.ACCEPTANCE_CRITERIA,
+      value: detailRefinedBacklog?.details.acceptanceCriteria || [],
+    },
+    {
+      type: DetailType.ADDICIONAL_NOTES,
+      value: detailRefinedBacklog?.details.addicionalNotes || "",
+    },
+  ];
 
   return (
     <Card
@@ -32,22 +52,15 @@ const BacklogFeatureDetail = () => {
         <CardHeader className="w-full h-full p-10">
           <CardTitle className="mb-3">{detailRefinedBacklog.summary}</CardTitle>
           <div className="pb-10">
-            <BacklogFeatureDetailItem
-              type={DetailType.BUSINESS_VALUE}
-              value={detailRefinedBacklog.details.businessValue}
-            />
-            <BacklogFeatureDetailItem
-              type={DetailType.DESCRIPTION}
-              value={detailRefinedBacklog.details.description}
-            />
-            <BacklogFeatureDetailItem
-              type={DetailType.ACCEPTANCE_CRITERIA}
-              value={detailRefinedBacklog.details.acceptanceCriteria}
-            />
-            <BacklogFeatureDetailItem
-              type={DetailType.ADDICIONAL_NOTES}
-              value={detailRefinedBacklog.details.addicionalNotes}
-            />
+            {backlogFeatureDetailList.map(
+              ({ type, value }: RefinedBacklogDetailList) => (
+                <BacklogFeatureDetailItem
+                  key={type}
+                  type={type}
+                  value={value}
+                />
+              )
+            )}
           </div>
         </CardHeader>
       ) : (

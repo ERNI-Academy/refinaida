f86013ec2f6dafbca3693c701cc41586703a8d20
Paul Lozano - Contractor <Paul.Lozano@contractor.qiagen.com>
2025-03-06 10:56:12 +0100

    fix: fix redirection header when stayed in the main view

--- src/components/shell.tsx
+++ src/components/shell.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
 
 import BackNewFeatureDialog from "@/features/shared/back-new-feature-dialog/back-new-feature-dialog";
+import { routes } from "@/router";
 
 type ShellProps = {
   children: React.ReactNode;
@@ -8,17 +10,22 @@ type ShellProps = {
 
 export const Shell = ({ children }: ShellProps) => {
   const { t } = useTranslation();
+  const location = useLocation();
 
   return (
     <div className="flex flex-col h-full w-full">
       <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4">
-        <BackNewFeatureDialog
-          trigger={
-            <h1 className="text-2xl uppercase font-mono cursor-pointer">
-              {t("header.title")}
-            </h1>
-          }
-        />
+        {location.pathname === routes.default ? (
+          <h1 className="text-2xl uppercase font-mono">{t("header.title")}</h1>
+        ) : (
+          <BackNewFeatureDialog
+            trigger={
+              <h1 className="text-2xl uppercase font-mono cursor-pointer">
+                {t("header.title")}
+              </h1>
+            }
+          />
+        )}
       </header>
       <main className="h-full w-full flex-1 bg-muted/40">{children}</main>
     </div>

c9df4b750f6a6cae033e9ec2017ccfa603e9cdf3
Paul Lozano <paul_lozano@hotmail.es>
2025-06-27 18:18:50 +0200

    style: change button, card and input components

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -9,6 +9,13 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, error, ...props }, ref) => {
+    const borderStyles = "border border-brand-secondary-95 rounded-md";
+    const disabledStyles = "disabled:cursor-not-allowed disabled:opacity-50";
+    const fileStyles =
+      "file:border-0 file:bg-transparent file:text-sm file:font-medium";
+    const focusStyles =
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 ring-offset-background focus-visible:ring-brand-primary";
+
     return (
       <div className="w-full">
         <input
@@ -16,7 +23,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           ref={ref}
           type={type}
           className={cn(
-            "flex h-10 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            "flex h-10 bg-background px-3 py-2 text-sm text-brand-secondary-10 placeholder:text-muted-foreground",
+            borderStyles,
+            fileStyles,
+            focusStyles,
+            disabledStyles,
             className
           )}
         />
